4e0039d3f5df1e44a1f556381d794a5caa736f10
Yesmean <ly_dinari@esi.dz>
2023-12-11 13:03:49 +0100

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,9 @@ function App() {
   return (
     <section className='bg-blue-400 flex'>
       <h1>Youcef is nuts</h1>
-      <p>
-        Consequat reprehenderit elit elit cupidatat nulla. 
-        Officia ullamco consectetur non occaecat exercitation commodo 
-        labore magna occaecat occaecat labore minim commodo.
-      </p>
+    
       
-      
+
     </section>
   );
 }
