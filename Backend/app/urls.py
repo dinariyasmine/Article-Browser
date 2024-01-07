a20275c3a47ca27e00d7fbd645529d8b069bae59
RAHOU Meriem <lm_rahou@esi.dz>
2024-01-07 21:02:52 +0100

    added search_and_filter_articles function + it's url

--- Backend/app/urls.py
+++ Backend/app/urls.py
@@ -1,9 +1,14 @@
 # app/urls.py
 from django.urls import path
 from .views import search_articles 
+from .views import search_and_filter_articles
 
 urlpatterns = [
     # ... your existing URL patterns
     path('search/', search_articles, name='search_articles'),
+
+    # mery's code
+    path('search_and_filter/', search_and_filter_articles, name='search_and_filter_articles'),
+
 
 ]
--- Backend/app/views.py
+++ Backend/app/views.py
@@ -36,3 +36,51 @@ def search_articles(request):
         # Handle the exception or log the error
         print(f"Error: {e}")
         return render(request, 'search_results.html', {'articles': [], 'query': query, 'error': str(e)})
+
+
+
+
+# mery's code
+# Path: Backend/app/views.py
+
+from datetime import datetime
+
+def search_and_filter_articles(request):
+    # Extract search query from the front end team
+    search_query = request.GET.get('q', '')
+
+    # Extract filter criteria from the front end team
+    authors_list = request.GET.getlist('authors', [])
+    keywords_list = request.GET.getlist('keywords', [])
+    institutions_list = request.GET.getlist('institutions', [])
+
+    from_date_str = request.GET.get('from_date', '')
+    to_date_str = request.GET.get('to_date', '')
+
+    # Convert date strings to datetime objects
+    from_date = datetime.strptime(from_date_str, '%Y-%m-%d') if from_date_str else None
+    to_date = datetime.strptime(to_date_str, '%Y-%m-%d') if to_date_str else None
+
+    try:
+        # Perform Elasticsearch search with retry mechanism
+        es_result = perform_elasticsearch_search(search_query)
+
+        # Extract the list of article IDs from the Elasticsearch response
+        es_article_ids = [hit.meta.id for hit in es_result]
+
+        # Apply additional filters based on the filter criteria
+        filtered_articles = Article.objects.filter(
+            id__in=es_article_ids,
+            authors__name__in=authors_list,
+            keywords__name__in=keywords_list,
+            institutions__name__in=institutions_list,
+            pub_date__gte=from_date,
+            pub_date__lte=to_date
+        ).distinct()
+
+        return render(request, 'search_results.html', {'articles': filtered_articles, 'query': search_query})
+
+    except Exception as e:
+        # Handle the exception or log the error
+        print(f"Error: {e}")
+        return render(request, 'search_results.html', {'articles': [], 'query': search_query, 'error': str(e)})
