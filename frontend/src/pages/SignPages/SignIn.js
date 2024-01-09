417aa3a9743df65fae63892cf3b61cf9a5bac8a0
Yesmean <ly_dinari@esi.dz>
2024-01-09 18:43:20 +0100

    Registration integration done

--- frontend/src/pages/SignPages/SignIn.js
+++ frontend/src/pages/SignPages/SignIn.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import TextFiled from '../../components/Signing/TextFiled/TextFiled';
 import LogInButton from '../../components/Signing/LogInButton/LogInButton';
 import Logo from '../../components/Signing/Logo/Logo';
@@ -8,35 +9,74 @@ import Sign from '../../components/Signing/Sign/Sign';
 import AuthImg from '../../assets/AuthImage.svg';
 import './Signing.css';
 
-
 const SignIn = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
- 
+  
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+    console.log("Username:",username);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  //Post request to login to the path : localhost/auth/login
+  const handleSubmit = async () => {
+    console.log("Username:", username);
+  
+    try {
+      const response = await axios.post(
+        'http://127.0.0.1:8000/auth/login/',
+        {
+          username: username,
+          password: password,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': window.csrfToken,
+          },
+          withCredentials: true,
+        }
+      );
+      console.log('Response:', response.data);
+      // Handle the response as needed
+    } catch (error) {
+      console.error('Error during login:', error.response.data);
+      console.log('Validation errors:', error.response.data.errors); 
+    }
+  };
+  
+  
+
 
   return (
     <div className='h-screen flex justify-between items-center'>
-        <div className='LeftSide'>
-          <div className='logo'>
-            <Logo/>
-          </div>
-          <img src={AuthImg} alt='Authentification' />        
-          <div className='slogan'>
-            <Slogan/>
-          </div>
+      <div className='LeftSide'>
+        <div className='logo'>
+          <Logo />
         </div>
-        <div className='RightSide'>
-            <AuthentificationTitle title='Log In' />
-            <div className='MRightSide'>
-              <TextFiled text='Email adress'/>
-              <TextFiled text='Password'/>
-            </div>
-            <div className='BRightSide'>
-              <LogInButton text='Log In'/>
-              <Sign account='Don t have an account?' sign='up'/>
-            </div>
+        <img src={AuthImg} alt='Authentification' />
+        <div className='slogan'>
+          <Slogan />
         </div>
+      </div>
+      <div className='RightSide'>
+        <AuthentificationTitle title='Log In' />
+        <div className='MRightSide'>
+          <TextFiled text='UserName' value={username} onChange={handleUsernameChange} />
+          <TextFiled text='Password' type='password' value={password} onChange={handlePasswordChange} />
+        </div>
+        <div className='BRightSide' onClick={handleSubmit}>
+          <LogInButton text='Log In' />
+          <Sign account="Don't have an account?" sign='up' />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
