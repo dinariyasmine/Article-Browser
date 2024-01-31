5f0ae67f1c4d8cb93826b935f78cdbed3b4e6450
brahimiThenina <lt_brahimi@esi.dz>
2024-01-31 19:22:58 +0100

    Tests unitaires et fonction addFavorites

--- Backend/app/urls.py
+++ Backend/app/urls.py
@@ -1,18 +1,10 @@
 # app/urls.py
 from django.urls import path
-from .views import search_articles 
-from .views import search_and_filter_articles, index_articles, create_sample_articles
+from .views import search_and_filter_articles, index_articles, search_articles,add_to_favorites
 
 urlpatterns = [
-    # ... your existing URL patterns
     path('search/', search_articles, name='search_articles'),
-
-    # mery's code
     path('search_and_filter/', search_and_filter_articles, name='search_and_filter_articles'),
-
-    path('index-articles/', index_articles, name='index_articles'),
-
-    path('create-sample-articles/', create_sample_articles, name='create_sample_articles'),
-
-
-]
+    path('index-articles/<int:article_id>/', index_articles, name='index_articles'),
+    path('add_to_favorites/<int:article_id>/', add_to_favorites, name='add_to_favorites'),
+    ]
